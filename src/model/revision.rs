35a167d7a880281d7528f93b00fbecab83e9417c
Runeville <varkhipov2004@gmail.com>
2026-09-07 19:48:01 +0700

    add more structs

--- src/model/revision.rs
+++ src/model/revision.rs
@@ -1,2 +1,39 @@
+use super::Cell;
+
 #[derive(Debug, Clone)]
-pub struct Revision {}
+pub enum RevisionRowColumnAction {
+    InsertRow,
+    DeleteRow,
+}
+
+#[derive(Debug, Clone)]
+pub enum RevisionChangeEnum {
+    RevisionRowColumn {
+        action: RevisionRowColumnAction,
+        address: String,
+    },
+    RevisionCellChange {
+        value: Cell,
+        address: String,
+    },
+    Formatting {
+        start: usize,
+        length: usize,
+        address: String,
+        action: String,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct RevisionChange {
+    index: usize,
+    sheet_id: usize,
+    change: RevisionChangeEnum,
+}
+
+#[derive(Debug, Clone)]
+pub struct Revision {
+    pub revision_id: usize,
+    pub user_name: String,
+    pub changes: Vec<RevisionChange>,
+}
