053301fd58e30425bb8192031a05e3c5703f6c26
Runeville <varkhipov2004@gmail.com>
2026-09-17 09:56:22 +0700

    make exports pub(super)

--- src/write/revision.rs
+++ src/write/revision.rs
@@ -1,9 +1,9 @@
 use crate::RevisionLog;
 
-pub fn revision_headers_xml(revision_logs: &[RevisionLog]) -> String {
+pub(super) fn revision_headers_xml(revision_logs: &[RevisionLog]) -> String {
     todo!()
 }
 
-pub fn revision_log_xml(revision_log: &RevisionLog) -> String {
+pub(super) fn revision_log_xml(revision_log: &RevisionLog) -> String {
     todo!()
 }
