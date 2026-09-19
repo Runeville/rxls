83ae07168acb6b48d11cd3fc549855f2dfd44c3b
Runeville <varkhipov2004@gmail.com>
2026-09-20 03:06:50 +0700

    fix min_rid

--- src/write/revision.rs
+++ src/write/revision.rs
@@ -10,13 +10,16 @@ pub(super) fn revision_headers_xml(revision_logs: &[RevisionLog]) -> String {
         let max_sheet_id = revision_log.n_sheets + 1;
 
         s.push_str(&format!(
-            r#"<header guid="{{{}}}" dateTime="{}" maxSheetId="{}" userName="{}" r:id="{}" minRId="{}">"#,
+            r#"<header guid="{{{}}}" dateTime="{}" maxSheetId="{}" userName="{}" r:id="{}" {}>"#,
             revision_log.guid,
             revision_log.date_time,
             max_sheet_id + 1,
             revision_log.user_name,
             revision_log.revision_log_id,
-            revision_log.min_rid.unwrap_or_default()
+            revision_log
+                .min_rid
+                .map(|id| format!(r#"minRId="{id}""#))
+                .unwrap_or_default()
         ));
         s.push_str(&format!(
             r#"<sheetIdMap count="{}">"#,
