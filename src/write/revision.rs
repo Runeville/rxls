4dc7bbbe140156eba1d983288b594d63ad7de6f2
Runeville <varkhipov2004@gmail.com>
2026-09-20 03:36:48 +0700

    fix max_sheet_id

--- src/write/revision.rs
+++ src/write/revision.rs
@@ -13,7 +13,7 @@ pub(super) fn revision_headers_xml(revision_logs: &[RevisionLog]) -> String {
             r#"<header guid="{{{}}}" dateTime="{}" maxSheetId="{}" userName="{}" r:id="{}" {} {}>"#,
             revision_log.guid,
             revision_log.date_time,
-            max_sheet_id + 1,
+            max_sheet_id,
             revision_log.user_name,
             revision_log.revision_log_id,
             revision_log
